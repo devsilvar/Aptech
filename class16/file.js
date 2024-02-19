3ccd035c903d0e1a168a6fb6fd11d6ecaa38fcd2
Yusuf <refsilva@yahoo.com>
2024-02-19 15:48:38 +0100

    added monclass

---
+++ class16/file.js
@@ -0,0 +1,18 @@
+if ( 10 ) {
+  console.log( 'object' );
+}
+let names = 'yusuf';
+if ( names === 'yusuf' ) {
+  console.log( 'object' );
+}
+
+function hello () {
+  console.log( 3 );
+}
+
+if ( names == 3 ) {
+  console.log( 'object' );
+}
+function ff () {
+  console.log( 'object' );
+}
