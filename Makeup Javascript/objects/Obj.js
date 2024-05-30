461dc8f3890a5a400f6635a849e16cd6f445b444
Yusuf <refsilva@yahoo.com>
2024-05-30 14:01:37 +0100

    Javscript objects

---
+++ Makeup Javascript/objects/Obj.js
@@ -0,0 +1,53 @@
+// let nameOfCar = prompt( "what is the name of Your Cars" )
+
+// let CarColor = prompt( "what is teh color of your car" )
+
+// let Tinted = prompt( "DO ou have a Tinted Glass" )
+
+// let CarPrice = prompt( "How much is your cars" )
+
+// let CarModel = prompt( "what is the model of your car" )
+
+// let CarEngine = prompt( "What Engine is your car using" )
+
+
+// let userAge = prompt( "Enter Your Age" )
+
+// const groupCar = {
+//     name: nameOfCar,
+//     color: CarColor,
+//     hasTintedGlass: Tinted,
+//     price: CarPrice,
+//     Engine: CarEngine,
+//     Model: CarModel,
+//     age: userAge
+// }
+
+// if ( groupCar.age > 20 ) {
+//     console.log( "You are not Eligible to Submit This Form" )
+// } else {
+//     console.log( "Submmitting Form" )
+// }
+
+
+const car = {
+    name: "Volvo",
+    color: "red",
+    hasTintedGlass: false,
+    price: 50000,
+    Engine: "v8",
+    Model: "volvo-2004"
+}
+
+
+const pupil = {}
+pupil.name = "Dele"
+pupil.age = 34
+pupil.gnder = "male"
+
+
+// car.name = "Volkwagen"
+// car.color = "green"
+
+console.log( car )
+console.log( pupil )
--- Makeup Javascript/objects/ReactObj.js
+++ Makeup Javascript/objects/ReactObj.js
@@ -80,3 +80,170 @@ let newSTudent2 = genJambStudentObj()
 
 //chnage all the keys to a new value
 
+// Object is a Javascript Data Type
+
+let shoes1 = {
+    productName: "Leather Shoes",
+    Productprice: 3000,
+    productColor: 'red',
+    productDetails: 'It is a Nice Porducts Crested By the best SHoes designer',
+    size: 43,
+}
+
+
+let shoes3 = {
+    productName: "RUbber Shoes",
+    Productprice: 7000,
+    productColor: 'pink',
+    productDetails: 'It is a Nice Porducts Crested By the best SHoes designer',
+    size: 34,
+}
+
+
+//Functional protoType or Factory Function
+
+function genrateProduct ( productName, productprice, productColor, productDetails, size ) {
+    return {
+        productName: productName,
+        productColor: productColor,
+        productprice: productprice,
+        productDetails: productDetails,
+        size: size,
+    }
+
+}
+
+let shoess1 = genrateProduct( "Tennis", 9000, 'white', "The shoe is a Nive and Affordable Shoes used fro runing", 44 )
+
+
+console.log( genrateProduct( "Nylon Shoes", 2000, 'orange', "The shoe is a Nive and Affordable Shoes used fro runing", 34 ) )
+
+
+// Nested Object
+// nested Object is when you have an Object inside another Object
+
+let customerrecords = {
+    name: "Dele",
+    gender: 'male',
+    score: {
+        ENG: 44,
+        MATH: 55,
+        CHEM: 44,
+    }
+}
+
+let customertransaction = {
+    name: "dele",
+    age: 12,
+    transactiton: 400000
+}
+// let joinedObj = student + pupil;
+// console.log( joinedObj )
+
+
+// let joinedObj = Object.assign( {}, student, pupil )
+
+// console.log( joinedObj )
+
+
+// The secnd Way we can jin Object is by iusing Array Desturcturing
+
+
+let joinObj = { ...customerrecords, ...customertransaction }
+
+console.log( joinObj )
+
+let keys = Object.keys( customerrecords )
+console.log( keys )
+
+let valuess = Object.values( customerrecords )
+console.log( valuess )
+
+
+
+
+let studentAGe = [ 2, 3, 4, 5, 5 ]
+
+
+
+// 1. Create an object to store your personal information such as name, age, and favorite hobby.
+// 2. Prompt the user to enter their name, age, and favorite hobby and store it in the object.
+// 3. Create a function to display the information in a formatted way.like this => My name is { name } and my hobby is { hobby } and i am { age }
+
+const car = {
+    name: "Volvo",
+    color: "red",
+    hasTintedGlass: false,
+    price: 50000,
+    Engine: "v8",
+    Model: "volvo-2004"
+}
+
+
+const state = {
+    used: false,
+}
+
+let name = prompt( "Enter your Name" )
+let age = prompt( "enter your age" )
+let hobby = prompt( "enter your Hobby" )
+
+const Ester = {
+    name: name,
+    age: age,
+    hobby: hobby
+}
+
+function fomatted () {
+    return `MY name is {Ester.name} and my Hobbby is ${Ester.hobby} and i am ${Ester.age}`
+}
+
+fomatted()
+
+
+
+
+
+let newCar = Object.assign( {}, car, state )
+
+// find out if this car is red
+
+newCar;
+
+
+
+
+
+
+
+// 4. Add a method to the object that allows the user to update any of the properties.
+// 5. Prompt the user to choose which property they want to update and call the corresponding method.
+
+
+
+
+
+
+for ( let i = 0; i <= keys.length; i++ ) {
+    console.log( keys[ i ] )
+}
+
+
+
+// Create a JavaScript object called "car" with the following properties: 
+name (string), 
+color (string), 
+horsepower (number),
+isElectric (boolean). 
+
+// Then, create a second object called "userInput" with prompts for the user to input the values for each property of the "car" object. 
+name (string), 
+color (string), 
+horsepower (number),
+isElectric (boolean). 
+
+
+// Finally, use the whatever method to merge the user's input into the "car" object.
+
+
+
